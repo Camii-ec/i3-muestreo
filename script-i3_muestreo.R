449b1f32d5d45d5c830860dab9fac6b548355d75
Vilca <fvilca@uc.cl>
2022-11-14 20:19:22 -0300

    blabla

--- script-i3_muestreo.R
+++ script-i3_muestreo.R
@@ -6,6 +6,15 @@ library(survey)
 
 N <- 1000
 
+
+
+# Cruce Primera vuelta v/s Segunda Vuelta
+
+sort(table(datos$P1v))
+sort(table(datos$P2v))
+cruce_1v_2v <- table(datos$P1v,datos$P2v)
+round(prop.table(cruce_1v_2v,1)*100,3)
+
 # Pregunta 2 --------------------------------------------------------------
 
 
