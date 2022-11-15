bacacb9b4bc33b00b4bddfd872c34a2db6ff1a65
Camila Echeverría <cecheverriac@uc.cl>
2022-11-14 21:19:06 -0300

    a

--- script-i3_muestreo.R
+++ script-i3_muestreo.R
@@ -1,11 +1,27 @@
 
 datos <- rio::import("Encuesta.xlsx")
 library(survey)
+library(tidyverse)
 
 # a) --------------------------------------------------------------
 
-N <- 1000
+#Primero se eligen las manzanas
+#Segundo se elige la vivienda
+#Al final se elige la persona
 
-# Pregunta 2 --------------------------------------------------------------
+datos$zona <- as.factor(datos$zona)
+
+aux <- matrix(table(datos$zona, datos$IdMz), nrow = 4)
+
+manzanas.zona <- rowSums(ifelse(aux!=0, 1, 0))
+
+## Probabilidades de selección de la vivienda
+
+datos <- datos %>% 
+  mutate(mxzona = manzanas.zona[zona]) %>% 
+  mutate(p.sel1 =  (1/mxzona) * (1/Nviv), #para la etapa 1 (vivienda)
+         p.sel2 = (1/Nelig)) #para la etapa 2 (persona)
+
+# b) --------------------------------------------------------------
 
 
